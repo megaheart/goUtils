1a488f79695182252faffaaadf0c9211dcdd7b7d
Tran Phuc Manh Linh <linh2501@outlook.com.vn>
2025-07-04 22:15:27 +0700

    =)

--- collectionHelper.go
+++ collectionHelper.go
@@ -3,7 +3,6 @@ package goUtils
 // import "crypto/rand"
 import (
 	"math/rand"
-	"time"
 )
 
 func PickRandom[T any](slice []T, n int) []T {
@@ -14,7 +13,23 @@ func PickRandom[T any](slice []T, n int) []T {
 	}
 	clone := make([]T, N)
 	copy(clone, slice)
-	random := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < n; i++ {
+		j := rand.Intn(N-i) + i
+		clone[i], clone[j] = clone[j], clone[i]
+	}
+
+	return clone[:n]
+}
+
+func PickRandomWithSeed[T any](slice []T, n int, seed int64) []T {
+	N := len(slice)
+	n = min(N, n)
+	if n < 1 {
+		return []T{}
+	}
+	clone := make([]T, N)
+	copy(clone, slice)
+	random := rand.New(rand.NewSource(seed))
 	for i := 0; i < n; i++ {
 		j := random.Intn(N-i) + i
 		clone[i], clone[j] = clone[j], clone[i]
