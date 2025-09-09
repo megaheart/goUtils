593a4fde230ea13b89299d3deca4fe0eb7c65307
Tran Phuc Manh Linh <linh2501@outlook.com.vn>
2025-09-09 20:14:59 +0700

    =))))

--- test_exec/main.go
+++ test_exec/main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"crypto/rand"
 	"fmt"
+	"math/big"
 
 	"github.com/megaheart/goUtils"
 )
+
+func Random(a, b int) int {
+	// Generate a random integer in [a, b)
+	n, _ := rand.Int(rand.Reader, big.NewInt(int64(b-a)))
+	return int(n.Int64()) + a
+}
 
 func main() {
 	s := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
@@ -12,7 +20,7 @@ func main() {
 	count := []int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
 	// Pick n random elements from s
 	for i := 0; i < 1000000; i++ {
-		picked := goUtils.PickRandom(s, n)
+		picked := goUtils.PickRandom(s, n, Random)
 
 		// fmt.Printf("%v\n", picked)
 		for _, v := range picked {
