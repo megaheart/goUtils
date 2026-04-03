61ecbf3ae0439d9a3edeecf0974c56fd5324eeb2
Tran Phuc Manh Linh <linh2501@outlook.com.vn>
2026-04-03 11:52:27 +0700

    fix param issue, unstable

--- appHelper/params.go
+++ appHelper/params.go
@@ -5,19 +5,22 @@ import (
 	"fmt"
 	"os"
 	"reflect"
+	"strconv"
 	"strings"
+
+	"github.com/jedib0t/go-pretty/v6/table"
 )
 
 // Setup name for application parameter by attribute tag `appParam:"<name>"` in struct field.
 // Example:
 //
 //	type AppParamSetup struct {
-//	    MaxUploadSize int    `appParam:"max-upload-size"`
-//	    LogLevel      string `appParam:"log-level"`
+//	    MaxUploadSize int    `appParam:"max-upload-size" appParamUsage:"max upload size in bytes"`
+//	    LogLevel      string `appParam:"log-level" appParamUsage:"log level for the application"`
 //	}
 //
 // Then use ParseParams to get parameters values from flag args or
-// env vars and fill AppParamSetup struct.
+// env vars and fill AppParamSetup struct. Environment variable is more priority than flag arg.
 //
 // App param name must be unique, formatted as kebab-case (e.g., "max-upload-size")
 // This setup make program auto-parse flag arg (params' names with "--" prefix,
@@ -29,53 +32,139 @@ import (
 func ParseParams[T any](defaultValue T, envVarNamePrefix string) (*T, error) {
 	result := &defaultValue
 	t := reflect.TypeOf(*result)
-	flagList := make([]*string, 0)
+	flagList := make([]interface{}, 0)
 	for i := 0; i < t.NumField(); i++ {
 		f := t.Field(i)
 		paramName := f.Tag.Get("appParam")
 		if paramName != "" {
-			flagList = append(flagList, flag.String(paramName, "", ""))
+			fv := reflect.ValueOf(result).Elem().FieldByName(f.Name)
+			if fv.IsValid() && fv.CanSet() {
+				switch fv.Kind() {
+				// String
+				case reflect.String:
+					defaultValue := fv.Interface().(string)
+					flagList = append(flagList, flag.String(paramName, defaultValue, f.Tag.Get("appParamUsage")))
+				// Int types
+				case reflect.Int:
+					defaultValue := fv.Interface().(int)
+					flagList = append(flagList, flag.Int64(paramName, int64(defaultValue), f.Tag.Get("appParamUsage")))
+				case reflect.Int8:
+					defaultValue := fv.Interface().(int8)
+					flagList = append(flagList, flag.Int64(paramName, int64(defaultValue), f.Tag.Get("appParamUsage")))
+				case reflect.Int16:
+					defaultValue := fv.Interface().(int16)
+					flagList = append(flagList, flag.Int64(paramName, int64(defaultValue), f.Tag.Get("appParamUsage")))
+				case reflect.Int32:
+					defaultValue := fv.Interface().(int32)
+					flagList = append(flagList, flag.Int64(paramName, int64(defaultValue), f.Tag.Get("appParamUsage")))
+				case reflect.Int64:
+					defaultValue := fv.Interface().(int64)
+					flagList = append(flagList, flag.Int64(paramName, defaultValue, f.Tag.Get("appParamUsage")))
+				// Float types
+				case reflect.Float32:
+					defaultValue := fv.Interface().(float32)
+					flagList = append(flagList, flag.Float64(paramName, float64(defaultValue), f.Tag.Get("appParamUsage")))
+				case reflect.Float64:
+					defaultValue := fv.Interface().(float64)
+					flagList = append(flagList, flag.Float64(paramName, defaultValue, f.Tag.Get("appParamUsage")))
+				// Bool type
+				case reflect.Bool:
+					defaultValue := fv.Interface().(bool)
+					flagList = append(flagList, flag.Bool(paramName, defaultValue, f.Tag.Get("appParamUsage")))
+				default:
+					return nil, fmt.Errorf("unsupported field type: %s", fv.Kind())
+				}
+			}
 		}
 	}
+
+	wantHelp := flag.Bool("help", false, "show help message")
 	flag.Parse()
+
+	if *wantHelp {
+		fmt.Printf("Usage of `%s`:\n", os.Args[0])
+		usageTable := table.NewWriter()
+		usageTable.SetOutputMirror(os.Stdout)
+		style := table.StyleDefault
+		style.Options.DrawBorder = false
+		style.Options.SeparateColumns = false
+		style.Options.SeparateHeader = false
+		style.Options.SeparateRows = false
+		usageTable.SetStyle(style)
+
+		usageTable.AppendRow(table.Row{"Flag", "Environment Variable", "Description"})
+		for i := 0; i < t.NumField(); i++ {
+			f := t.Field(i)
+			paramName := f.Tag.Get("appParam")
+
+			if paramName != "" {
+				paramUsage := f.Tag.Get("appParamUsage")
+				flagName := "--" + paramName
+				envName := getEnvVarNameFromAppParamName(paramName, envVarNamePrefix)
+				usageTable.AppendRow(table.Row{flagName, envName, paramUsage})
+			}
+		}
+		usageTable.Render()
+		os.Exit(0)
+	}
 
 	for i := 0; i < t.NumField(); i++ {
 		f := t.Field(i)
 		paramName := f.Tag.Get("appParam")
 		if paramName != "" {
-			// get value from flag
-			paramValue := *flagList[i]
-			if paramValue == "" {
-				// try to get from env var if flag not set
-				envVarName := getEnvVarNameFromAppParamName(paramName, envVarNamePrefix)
-				paramValue = os.Getenv(envVarName)
-			}
-
-			if paramValue == "" {
-				continue
-			}
-
+			envVarName := getEnvVarNameFromAppParamName(paramName, envVarNamePrefix)
 			fv := reflect.ValueOf(result).Elem().FieldByName(f.Name)
 			if fv.IsValid() && fv.CanSet() {
 				switch fv.Kind() {
 				case reflect.String:
-					fv.SetString(paramValue)
+					if paramValue, ok := os.LookupEnv(envVarName); ok {
+						fv.SetString(paramValue)
+						continue
+					}
+					paramValueRef := flagList[i].(*string)
+					if paramValueRef != nil {
+						fv.SetString(*paramValueRef)
+					}
 				case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-					// convert string to int
-					var intVal int64
-					count, err := fmt.Sscanf(paramValue, "%d", &intVal)
-					if err != nil || count != 1 {
-						return nil, fmt.Errorf("invalid int value for param '%s': %s", paramName, paramValue)
+					if paramValue, ok := os.LookupEnv(envVarName); ok {
+						intVal, err := strconv.ParseInt(paramValue, 10, 64)
+						if err != nil {
+							return nil, fmt.Errorf("invalid int value for param '%s': '%s'", paramName, paramValue)
+						}
+						fv.SetInt(intVal)
+						continue
 					}
-					fv.SetInt(intVal)
+					paramValueRef := flagList[i].(*int64)
+					if paramValueRef != nil {
+						fv.SetInt(*paramValueRef)
+					}
+				case reflect.Float32, reflect.Float64:
+					if paramValue, ok := os.LookupEnv(envVarName); ok {
+						floatVal, err := strconv.ParseFloat(paramValue, 64)
+						if err != nil {
+							return nil, fmt.Errorf("invalid float value for param '%s': '%s'", paramName, paramValue)
+						}
+						fv.SetFloat(floatVal)
+						continue
+					}
+					paramValueRef := flagList[i].(*float64)
+					if paramValueRef != nil {
+						fv.SetFloat(*paramValueRef)
+					}
 				case reflect.Bool:
-					// convert string to bool
-					var boolVal bool
-					count, err := fmt.Sscanf(paramValue, "%t", &boolVal)
-					if err != nil || count != 1 {
-						return nil, fmt.Errorf("invalid bool value for param '%s': %s", paramName, paramValue)
+					if paramValue, ok := os.LookupEnv(envVarName); ok {
+						// convert string to bool
+						boolVal, err := strconv.ParseBool(paramValue)
+						if err != nil {
+							return nil, fmt.Errorf("invalid bool value for param '%s': '%s'", paramName, paramValue)
+						}
+						fv.SetBool(boolVal)
+						continue
 					}
-					fv.SetBool(boolVal)
+					paramValueRef := flagList[i].(*bool)
+					if paramValueRef != nil {
+						fv.SetBool(*paramValueRef)
+					}
 				// Add other types if needed
 				default:
 					return nil, fmt.Errorf("unsupported field type: %s", fv.Kind())
--- go.mod
+++ go.mod
@@ -3,15 +3,24 @@ module github.com/megaheart/goUtils
 go 1.23.3
 
 require (
+	github.com/fsnotify/fsnotify v1.8.0
+	github.com/natefinch/lumberjack v2.0.0+incompatible
+	github.com/spf13/afero v1.15.0
+	github.com/stretchr/testify v1.10.0
+	go.uber.org/zap v1.27.0
+	golang.org/x/sys v0.30.0
+)
+
+require (
+	github.com/BurntSushi/toml v1.6.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/fsnotify/fsnotify v1.8.0 // indirect
-	github.com/natefinch/lumberjack v2.0.0+incompatible // indirect
+	github.com/jedib0t/go-pretty/v6 v6.7.9 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/spf13/afero v1.15.0 // indirect
-	github.com/stretchr/testify v1.10.0 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
-	go.uber.org/zap v1.27.0 // indirect
-	golang.org/x/sys v0.13.0 // indirect
 	golang.org/x/text v0.28.0 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
